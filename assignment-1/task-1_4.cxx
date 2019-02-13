d8a5074a51622890a56572277ed29abc6599a773
Ilya Pikulin <ilya.pikulin@gmail.com>
2019-02-13 22:05:07 +0300

    Solve 1_4

--- assignment-1/task-1_4.cxx
+++ assignment-1/task-1_4.cxx
@@ -12,9 +12,30 @@ n ≤ 10^7.
 ****************************************************************************/
 
 #include <iostream>
+#include <cmath>
+#include <limits>
+#include <numeric>
+#include <cstdint>
+#include <utility>
 
-int main()
-{
-    return 0;
+int main() {
+	uint64_t N; std::cin >> N;
+	// N = K * (A + B)
+	// Let's bruteforce the sum A + B
+
+	uint64_t multiplier = -1;
+	if (N % 2 == 0) {
+		multiplier = 2;
+		goto finish;
+	}
+	for (int i = 3; i <= std::sqrt(N); ++i) {
+		if (N % i == 0) {
+			multiplier = i;
+			goto finish;
+		}
+	}
+	multiplier = N;
+finish:
+	uint64_t K = N / multiplier;
+	std::cout << K * 1 << " " << K * (multiplier - 1);
 }
-
